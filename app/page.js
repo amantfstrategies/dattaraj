3a9eae976cdbfab47d0baf6f922e710608038dad
amantfstrategies <git config --global user.email>
2024-12-17 10:51:01 +0530

    home page image

--- app/page.js
+++ app/page.js
@@ -2,15 +2,21 @@ import React from "react";
 import { SlArrowDown } from "react-icons/sl";
 import WhyChooseUs from "./components/WhyChooseUs";
 import CarsCarousel from "./components/CarsCarousel";
-
+import Image from "next/image";
 
 function Page() {
   return (
     <div>
-      <div
-        className="relative w-full h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url('./bgimage.jpg')" }}
-      >
+      <div className="relative w-full h-screen">
+        <Image
+          src="/bgimage.jpg" 
+          alt="Background"
+          layout="fill" 
+          objectFit="cover" 
+          objectPosition="center" 
+          priority
+        />
+
         <div className="absolute inset-0 bg-black opacity-30"></div>
 
         <div className=" mt-20 absolute inset-0 flex flex-col justify-center items-center text-center text-white space-y-4">
@@ -25,35 +31,29 @@ function Page() {
               className="px-12 w-full py-2 appearance-none h-fit rounded text-gray-500 focus:outline-none focus:ring-0"
               value="All cars"
             >
-              <SlArrowDown className="text-black"/>
+              <SlArrowDown className="text-black" />
               <option>All cars </option>
               <option>Audi</option>
               <option>BMW</option>
-
-              
             </select>
             <select
               className="px-12 w-full py-2 appearance-none h-fit rounded text-gray-500 focus:outline-none focus:ring-0"
               value="All cars"
             >
-              <SlArrowDown className="text-black"/>
+              <SlArrowDown className="text-black" />
               <option>All cars </option>
               <option>Audi</option>
               <option>BMW</option>
-
-              
             </select>
 
             <select
               className="px-12 w-full py-2 appearance-none h-fit rounded text-gray-500 focus:outline-none focus:ring-0"
               value="All cars"
             >
-              <SlArrowDown className="text-black"/>
+              <SlArrowDown className="text-black" />
               <option>All cars </option>
               <option>Audi</option>
               <option>BMW</option>
-
-              
             </select>
 
             <button className="px-12 w-full py-2 bg-[#04DBC0] text-white rounded hover:bg-[#03a39c] transition duration-300">
@@ -63,8 +63,8 @@ function Page() {
         </div>
       </div>
 
-      <WhyChooseUs/>
-      <CarsCarousel/>
+      <WhyChooseUs />
+      <CarsCarousel />
     </div>
   );
 }
